4b2094ce7f9e13ea1504c98c9e37358eea5754ff
Nam Ki Hyeok <carpediem7490@gmail.com>
2022-12-13 22:16:53 +0900

    asd

---
+++ src/main/java/com/org/connect/jdbcexam/dao/RoleDao.java
@@ -0,0 +1,63 @@
+package com.org.connect.jdbcexam.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.org.connect.jdbcexam.Role;
+
+public class RoleDao {
+	 private static String dburl = "null";
+	 private static String dbUser = "null";
+	 private static String dbpasswd = "null";
+	
+	public Role getRole(Integer roleId) {
+		Role role = null;
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); //메모리에 driver 로드
+			conn = DriverManager.getConnection(dburl,dbUser,dbpasswd); //db연결
+			String sql = "SELECT role_id, description FROM role WHERE role_id = ?"; //sql
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, roleId);
+			rs = ps.executeQuery();
+			if(rs.next()) {
+				int id = rs.getInt(1);
+				String description = rs.getString(2);
+				role = new Role(id, description);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(conn!=null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(ps!=null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return role;
+	}
+	
+}
